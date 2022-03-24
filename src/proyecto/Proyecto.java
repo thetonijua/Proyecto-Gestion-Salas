65d10489a41d087cba5284d8488ad60dcf851598
UnaCuentaFalsaMas <garahxp@gmail.com>
2022-03-24 19:33:09 -0300

    Cesar
    Agregar Datos

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -18,8 +18,28 @@ public class Proyecto {
         // TODO code application logic here
         Curso cursos = new Curso();
         cursos.setCodigo("1M-A");
-        cursos.setPorceAprobacion(1);
-        System.out.println(cursos.getCodigo());
+        float porce = (float) 0.6;//no se porque me reconoce el 0.6 como double ayuda
+        cursos.setPorceAprobacion(porce);
+        cursos.setTotAlumn(1);
+        //preparando cursos.setAño
+        Dia[][] año = new Dia[12][31];
+        año[0][0] = new Dia();
+        año[0][0].setDiaContable(true);
+        año[0][0].setNumDia("01-01-2022");
+        //prepareando año[0][0].setAlumnos(alumnos)
+        Alumno[] alumnos = new Alumno[1];
+        alumnos[0] = new Alumno();
+        alumnos[0].setAsistenciaTot(true, año[0][0].getNumDia());
+        alumnos[0].setId(1);
+        alumnos[0].setNombre("Cesar");
+
+        año[0][0].setAlumnos(alumnos);
+        cursos.setAño(año);
+
+        System.out.println(cursos.getCodigo() + " Asistencia minima " + 100*cursos.getPorceAprobacion() + "% Total alumnos: " + cursos.getTotAlumn());
+        System.out.println("Curso:");
+        System.out.println(alumnos[0].getId()+" "+alumnos[0].getNombre()+" Asistencia total "+alumnos[0].getAsistenciaTot()+" Fecha: "+alumnos[0].getFechaAsis()[0]);
+        
 
     }
 
