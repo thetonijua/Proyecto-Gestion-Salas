a64a2593dfffc0ed9cee6194583333aa61235cb3
G-ODgithub <71039888+G-ODgithub@users.noreply.github.com>
2022-05-01 16:11:22 -0500

    Actualizados comentarios
    Actualizados comentarios de alumno, curso, dia y proyecto

---
+++ src/proyecto/Alumno.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+import java.util.*;
+
+/**
+ *
+ * @author BruceLee
+ */
+public class Alumno {
+
+    //variables de instancia
+    private String nombre;
+    private int id;
+    private int totAsistencia;
+    private ArrayList<Dia> asistencia;
+
+    //constructores
+    public Alumno() {
+        nombre = "No-Name";
+        id = 0;
+        totAsistencia = 0;
+        asistencia = new ArrayList();
+    }
+
+    public Alumno(String name, int ide) {
+        nombre = name;
+        id = ide;
+        totAsistencia = 0;
+        asistencia = new ArrayList();
+    }
+
+    //setters y getters de las variables
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTotAsistencia() {
+        return totAsistencia;
+    }
+
+    public void setTotAsistencia(int totAsistencia) {
+        this.totAsistencia = totAsistencia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    //métodos de la clase
+    /*dos métodos para añadir a la lista de asistencia una fecha y si el alumno asistió o no esa fecha
+    el primero es para una fecha de tipo String*/
+    public void asistencia(String fecha, String siNo) {
+        Dia aux = new Dia(fecha, siNo);
+
+        asistencia.add(aux);
+
+    }
+
+    //el segundo es para una fecha con su respectivo día, mes y año, todos de tipo int
+    public void asistencia(int dia, int mes, int anio, boolean siNo) {
+        // Dia aux=new Dia (dia,mes,anio,siNo);
+        asistencia.add(new Dia(dia, mes, anio, siNo));
+
+    }
+
+    //elimina un día de la lista
+    public void eliminarDia(String dd) {
+        for (int i = 0; i < asistencia.size(); i++) {
+            if (dd.equals(asistencia.get(i).getFecha())) {
+                if ("presente".equals(asistencia.get(i).getAsistencia())) {
+                    totAsistencia--;
+                }
+                asistencia.remove(i);
+            }
+        }
+    }
+
+    //imprime toda la asistencia del alumno
+    public void printAsistencia() {
+        for (int i = 0; i < asistencia.size(); i++) {
+            Dia aux = asistencia.get(i);
+            aux.printDia();
+        }
+    }
+
+}
