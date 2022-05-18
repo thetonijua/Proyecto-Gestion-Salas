0a63c5f5ab764055505e03258ad8c44ead1d242e
Juan Ignacio <juan.araya.h01@mail.pucv.cl>
2022-05-18 14:23:14 -0400

    Int grafica
    se saca interfaz grafica

--- src/proyecto/Curso.java
+++ src/proyecto/Curso.java
@@ -4,8 +4,6 @@
  * and open the template in the editor.
  */
 package proyecto;
-import javax.swing.*;
-import java.awt.event.*;
 import java.util.*;
 import java.io.*;
 
@@ -14,7 +12,7 @@ import java.io.*;
  *
  * @author BruceLee
  */
-public class Curso extends JFrame implements ActionListener, ItemListener {
+public class Curso {
 
     //variables de instancia
     private String nombre;
@@ -23,13 +21,8 @@ public class Curso extends JFrame implements ActionListener, ItemListener {
     private Map<Integer, Alumno> estudiantes;
     private Map<String, Integer> nombreXId;
     private List<Integer> listaEstu;
-    private Colegio c;
     BufferedReader scan = new BufferedReader(new InputStreamReader(System.in)); //usado para leer la lista
     
-    private JLabel label1,label2,label3,label4;
-    private JButton boton1,boton2,boton3,boton4,boton5;
-    private JTextField tf1,tf2;
-    private JComboBox combo1,combo2;
 
     //constructores
     public Curso(String name, float numero, int cod) {
@@ -219,143 +212,5 @@ public class Curso extends JFrame implements ActionListener, ItemListener {
            int cod = listaEstu.get(num);
            return estudiantes.get(cod);   
     }
-    
-    
-    
-    //
-    //      Metodos ventana
-    //
-    //
-    
-    
-    public void configVentana(Colegio c){
-        setLayout(null);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.c=c;
-        setTitle("Curso "+nombre+" (test version)");
-        label1= new JLabel("Nombre");
-        label2= new JLabel("Id");
-        label3= new JLabel("Lista Alumnos");
-        label4= new JLabel("Lista Dias");
-        boton1= new JButton("Agregar alumno");
-        boton2= new JButton("Eliminar");
-        boton3= new JButton("Eliminar dia");
-        boton4= new JButton("Pasar Lista");
-        boton5= new JButton("Atras");
-        tf1= new JTextField();
-        tf2= new JTextField();
-        combo1= new JComboBox();
-        combo2= new JComboBox();
-    
-    
-        
-        add(tf1);
-        add(tf2);
-
-        add(label2);
-        add(label1);
-        add(label3);
-        add(label4);
-   
-        add(boton1);
-        add(boton2);
-        add(boton3);
-//        add(boton4);
-        add(boton5);
-        add(combo1);
-        add(combo2);
-        
-        tf1.setBounds(200,60,150,17);
-        tf2.setBounds(200,100,150,17);
-
-        label1.setBounds(200,0,100,100);
-        label2.setBounds(200,40,100,100);
-        label3.setBounds(385,40,150,100);
-        label4.setBounds(250,205,150,100);
-        combo1.setBounds(385,100,150,19);
-        combo2.setBounds(250,263,150,19);
-       
-        boton1.setBounds(20,60,150,25);
-        boton2.setBounds(385,40,150,25);
-        boton3.setBounds(20,260,150,25);
-        boton4.setBounds(20,300,150,25);
-        boton5.setBounds(20,450,100,25);
-        
-        
-        boton1.addActionListener(this);
-        boton2.addActionListener(this);
-        boton3.addActionListener(this);
-        boton5.addActionListener(this);
-        combo1.addItemListener(this);
-        combo2.addItemListener(this);
-        
-        
-        
-        this.setBounds(0, 0, 570, 570);
-        this.setLocationRelativeTo(null);
-        this.setResizable(false);
-        this.setVisible(true);
-        
-    }
-    
-    
-    @Override
-    public void actionPerformed(ActionEvent e){
-        
-        if(e.getSource()==boton1){
-            String texto1= tf1.getText();
-            String texto2= tf2.getText();
-
-            if((texto1.equals(""))||(texto2.equals(""))){
-                JOptionPane.showMessageDialog(null, "Rellena ambos campos");
-            }else{
-                this.putAlumno(texto1, texto2);
-                combo1.addItem(texto1);
-                tf1.setText("");
-                tf2.setText("");
-            }
-   
-        }    
-        
-        if(e.getSource()==boton2){
-            if(combo1.getSelectedItem()==null){
-              JOptionPane.showMessageDialog(null, "Selecciona un alumno");  
-            }else{
-                String aux=combo1.getSelectedItem().toString();
-                eliminarAlumno(aux);
-                combo1.removeItem(aux);
-            }
-        }
-        
-        if(e.getSource()==boton3){
-            if(combo1.getSelectedItem()==null||combo2.getSelectedItem()==null){
-                JOptionPane.showMessageDialog(null, "Selecciona un día y un Alumno");  
-            }else{
-                String aux=combo1.getSelectedItem().toString();
-                Alumno a=this.getAlumno(aux);
-                a.eliminarDia(combo2.getSelectedItem().toString());
-            }
-        }
-        
-        if(e.getSource()==boton5){
-            this.setVisible(false);
-            c.setVisible(true);
-            
-        }
-        
-    }
-        
-    
-    @Override
-    public void itemStateChanged(ItemEvent e){
-            if(e.getSource()==combo1){
-                String selec= combo1.getSelectedItem().toString();
-                if(selec.equals("")){
-                    //JOptionPane.showMessageDialog(null, "Selecciona un curso");
-                }
-        }
-    }
-    
-    
-    
+ 
 }
